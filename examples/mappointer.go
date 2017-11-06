7e460196fc4218dfa999b2321b67820e06f10fab
shentao <tao.shen@transwarp.io>
2017-11-06 15:19:05 +0800

    fix mappointer

--- examples/mappointer.go
+++ examples/mappointer.go
@@ -4,15 +4,23 @@ import "fmt"
 
 func main() {
     mapA := make(map[string]string)
-    MapTestA := MapTest{
+    MapTestA := &MapTest{
         test: mapA,
     }
 
+    if a, ok := mapA["3"]; !ok {
+        fmt.Println(a)
+    }
+
     MapTestA.test["1"] = "1"
+
+    for key, value := range mapA {
+        mapA[key] = value + "2"
+    }
 
     fmt.Println(mapA)
 }
 
 type MapTest struct {
-    test map[string]string
+    test map[string]string `json:"-"`
 }
